58f0761f688132065281868c6b12fa4782f60309
Liu Cong <innerr@gmail.com>
2022-01-29 23:25:41 +0800

    fix helper importing
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- helper.bash
+++ helper.bash
@@ -1,4 +1,5 @@
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/string.bash"
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/ssh.bash"
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/os_cmd.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/mysql.bash"
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/ticat.bash"
