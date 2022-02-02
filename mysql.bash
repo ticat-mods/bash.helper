88ebfe94b399f87000864a2b5b91363f329bf000
Liu Cong <innerr@gmail.com>
2022-02-03 05:44:41 +0800

    support format in mysql execute helper
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- mysql.bash
+++ mysql.bash
@@ -5,7 +5,25 @@ function my_exe()
 	local user="${3}"
 	local db="${4}"
 	local query=`echo "${5}" | awk '$1=$1'`
-	mysql -h "${host}" -P "${port}" -u "${user}" --database="${db}" -e "${query}"
+
+	local fmt=''
+	if [ ! -z "${6+x}" ]; then
+		local fmt="${6}"
+	fi
+	if [ "${fmt}" == 'v' ]; then
+		local fmt='--vertical'
+	fi
+	if [ "${fmt}" == 'tab' ]; then
+		local fmt='--batch'
+	fi
+	if [ "${fmt}" == 't' ]; then
+		local fmt='--table'
+	fi
+	if [ -z "${fmt}" ]; then
+		local fmt='--table'
+	fi
+
+	mysql -h "${host}" -P "${port}" -u "${user}" --database="${db}" --comments ${fmt} -e "${query}"
 }
 
 function my_ensure_db()
