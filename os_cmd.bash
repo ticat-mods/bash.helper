3fb199377e7166b88a9965d96de425159e60d795
Liu Cong <innerr@gmail.com>
2022-02-16 17:47:18 +0800

    tool: add_bin_dir_to_sys_path
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- os_cmd.bash
+++ os_cmd.bash
@@ -40,3 +40,31 @@ function check_or_install()
 		echo "[:)] command ${to_check} installed"
 	fi
 }
+
+# Refrence: https://stackoverflow.com/questions/14637979/how-to-permanently-set-path-on-linux-unix
+function add_bin_dir_to_sys_path()
+{
+	local bin_dir="${1}"
+
+	local shell=$(echo ${SHELL} | awk 'BEGIN {FS="/";} { print $NF }')
+	echo "detected shell: ${shell}"
+	if [ -f "${HOME}/.${shell}_profile" ]; then
+		local profile=${HOME}/.${shell}_profile
+	elif [ -f "${HOME}/.${shell}_login" ]; then
+		local profile=${HOME}/.${shell}_login
+	elif [ -f "${HOME}/.${shell}rc" ]; then
+		local profile=${HOME}/.${shell}rc
+	else
+		local profile=${HOME}/.profile
+	fi
+	echo "shell profile:  ${profile}"
+
+	case :${PATH}: in
+		*:${bin_dir}:*) : "PATH already contains ${bin_dir}" ;;
+		*)  printf '\nexport PATH=%s:$PATH\n' "${bin_dir}" >> "${profile}"
+			echo "${profile} has been modified to add '${bin_dir}' to PATH"
+			;;
+	esac
+
+	echo "installed path: $bin_dir"
+}
