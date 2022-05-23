0b7bb0c914eac32bfbf32aaf645fb3937823e70f
Liu Cong <innerr@gmail.com>
2022-05-23 00:34:29 -0400

    add function `guess_ip`
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- ip.bash
+++ ip.bash
@@ -1,7 +1,14 @@
 function get_ips()
 {
-	ifconfig | { grep 'en[0123456789]\|eth[0123456789]\|wlp[0123456789]\|em[0123456789]' -A 3 || test $? = 1; } | \
-		{ grep -v inet6 || test $? = 1; } | { grep inet || test $? = 1; } | { grep -i mask || test $? = 1; } | awk '{print $2}'
+	ifconfig | { grep 'en[0123456789]\|eth[0123456789]\|wlp[0123456789]\|em[0123456789]\|inet[0123456789]\|inet' -A 3 || test $? = 1; } | \
+		{ grep -v '127.0.0.1' || test $? = 1; } | { grep -v 'localhost' || test $? = 1; } | \
+		{ grep -v '255.255.0.0' || test $? = 1; } | { grep -v '255.0.0.0' || test $? = 1; } | { grep -v '255.255.255.255' || test $? = 1; } | \
+		{ grep -v 'inet6' || test $? = 1; } | { grep 'inet ' || test $? = 1; } | { grep -i mask || test $? = 1; } | awk '{print $2}'
+}
+
+function guess_ip()
+{
+	get_ips | awk -F '.' '{print $1" "$2" "$3" "$4}' | sort -nr | awk '{print $1"."$2"."$3"."$4}' | head -n 1
 }
 
 function get_ips_cnt()
