a654b6e91623e470d52c1a5751d8c8ed4183bc1a
Liu Cong <innerr@gmail.com>
2022-07-20 19:41:56 +0800

    add `extract_lines_section`
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- string.bash
+++ string.bash
@@ -65,3 +65,23 @@ function normalize_github_addr()
 	fi
 	echo "https://github.com/${addr}"
 }
+
+function extract_lines_section()
+{
+	local lines="${1}"
+	local section="${2}"
+	local next_sect=''
+	if [ ! -z "${3+x}" ]; then
+		local next_sect="${3}"
+	fi
+	local lines=`echo "${lines}" |\
+		{ grep -A 999999 "${section}" || test $? = 1; } |\
+		{ grep -v "${section}" || test $? = 1; } |\
+		{ grep -v '\-\-\-\-' || test $? = 1; }`
+	if [ ! -z "${next_sect}" ]; then
+		local lines=`echo "${lines}" |\
+			{ grep -B 999999 "${next_sect}" || test $? = 1; } |\
+			{ grep -v "${next_sect}" || test $? = 1; }`
+	fi
+	echo "${lines}"
+}
